edf7d1c5dbce2be9e0520aad5bad6e193f6a31dd
Tsuyoshi Matsuzaki <tsmatsuz@microsoft.com>
2023-10-27 14:18:26 +0900

    to ubuntu20.04

--- Malmo_Maze_Sample/custom_malmo_env/shell_files/launchClient_headless.sh
+++ Malmo_Maze_Sample/custom_malmo_env/shell_files/launchClient_headless.sh
@@ -1,5 +1,5 @@
-#!/bin/bash ##
-##
-VAR_PATH="${MALMO_XSD_PATH}/../Minecraft" ##
-cd $VAR_PATH ##
-xvfb-run -a -e /dev/stdout -s '-screen 0 640x480x16' ./launchClient.sh -port $1 ##
+#!/bin/bash
+
+VAR_PATH="${MALMO_XSD_PATH}/../Minecraft"
+cd $VAR_PATH
+xvfb-run -a -e /dev/stdout -s '-screen 0 640x480x16' ./launchClient.sh -port $1
